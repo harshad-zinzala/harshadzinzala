53e1fa00136a38936bdb4dd76846d1e6affccf2d
Harshad Zinzala <145487247+harshad-zinzala@users.noreply.github.com>
2026-06-05 18:10:27 +0530

    Change favicon from FV.jpg to FV.png

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { router } from "./routes/Router";
 import { RouterProvider } from "react-router-dom";
-import "../src/../index.css";
 
 // Set favicon from bundled asset (src/assets/FV.jpg)
 try {
-  const faviconUrl = new URL("./assets/FV.jpg", import.meta.url).href;
+  const faviconUrl = new URL("./assets/FV.png", import.meta.url).href;
   let link = document.querySelector("link[rel~='icon']");
   if (!link) {
     link = document.createElement('link');
